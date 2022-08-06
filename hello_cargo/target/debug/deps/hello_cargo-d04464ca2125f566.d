e7ebeb9ff2e804abb10a45bb643cfc21e961aae6
MrRain1 <b.degliuberti@outlook.com>
2022-08-06 10:12:54 +0200

    Start

---
+++ hello_cargo/target/debug/deps/hello_cargo-d04464ca2125f566.d
@@ -0,0 +1,5 @@
+/home/mrrain19/repos/RustLearningJourney/hello_cargo/target/debug/deps/hello_cargo-d04464ca2125f566.rmeta: src/main.rs
+
+/home/mrrain19/repos/RustLearningJourney/hello_cargo/target/debug/deps/hello_cargo-d04464ca2125f566.d: src/main.rs
+
+src/main.rs:
